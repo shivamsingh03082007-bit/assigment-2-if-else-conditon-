3499602a6e4b21ead3afe74bda82d344ecbdbc02
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-09-28 18:27:13 +0530

    Create Calculate net salary.c

---
+++ Calculate net salary.c
@@ -0,0 +1,20 @@
+int main() {
+    float gross, allowance, deduction, net;
+    printf("Enter Gross Salary: ");
+    scanf("%f", &gross);
+
+    if(gross > 10000) {
+        allowance = gross * 0.10;
+        deduction = gross * 0.03;
+    } else if(gross > 5000) {
+        allowance = gross * 0.07;
+        deduction = gross * 0.02;
+    } else {
+        allowance = deduction = 0;
+    }
+
+    net = gross + allowance - deduction;
+
+    printf("Net Salary = %.2f\n", net);
+    return 0;
+}
